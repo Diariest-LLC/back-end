ef1e11e513214633885c766eb55eec44d307c6ac
TNYCL <tunaycl@outlook.com>
2022-12-24 04:01:38 +0300

    feature: DBE-14 requiredData variable and check method added.

--- src/main/java/com/diariest/server/netty/handler/WebSocketHandler.java
+++ src/main/java/com/diariest/server/netty/handler/WebSocketHandler.java
@@ -2,14 +2,23 @@ package com.diariest.server.netty.handler;
 
 import com.diariest.server.request.RequestAdapter;
 import com.diariest.server.request.modules.RequestModule;
+import com.diariest.server.request.providers.Login;
+import com.diariest.server.request.providers.Test;
+import com.diariest.server.utils.UtilConsole;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import org.json.JSONObject;
 
+import java.util.Arrays;
 import java.util.ConcurrentModificationException;
+import java.util.List;
 
 public class WebSocketHandler extends ChannelInboundHandlerAdapter {
+
+    public static List<String> requiredData = Arrays.asList(
+            "request_type"
+    );
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
@@ -18,8 +27,16 @@ public class WebSocketHandler extends ChannelInboundHandlerAdapter {
         JSONObject data;
         try {
             data = new JSONObject(((TextWebSocketFrame) msg).text());
-        } catch (Exception exception) {
+        } catch (Exception ex) {
             return;
+        }
+
+        for(String required : requiredData) {
+            try {
+                data.get(required);
+            } catch (Exception ex) {
+                return;
+            }
         }
 
         RequestModule requestModule = RequestAdapter.getModule(data.get("request_type").toString());
