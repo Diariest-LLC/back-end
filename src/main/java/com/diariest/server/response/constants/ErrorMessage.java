68d9df967e5d08f7b470534096cdc392bedcf7f1
TNYCL <tunaycl@outlook.com>
2023-01-11 16:50:57 +0300

    feature: DBE-29 Security methods included.

--- src/main/java/com/diariest/server/response/constants/ErrorMessage.java
+++ src/main/java/com/diariest/server/response/constants/ErrorMessage.java
@@ -4,12 +4,33 @@ import com.diariest.server.response.handlers.IMessageHandler;
 
 public enum ErrorMessage implements IMessageHandler {
 
-    NO_SESSION_DATA(201, "Wrong session data."),
-    MISSING_DATA(202, "Missing object"),
-    LIMITED_REQUEST(203, "You have been restricted for exceeding the request limit.");
+    NO_SESSION_DATA(
+            201,
+            "Wrong session data."
+    ),
+    MISSING_DATA(
+            202,
+            "Missing object."
+    ),
+    LIMITED_REQUEST(
+            203,
+            "You have been restricted for exceeding the request limit."
+    ),
+    USER_NICKNAME_ALREADY_USE(
+            204,
+            "This user nickname already using."
+    ),
+    EMAIL_ALREADY_USE(
+            205,
+            "This e-mail address already using."
+    ),
+    PHONE_NUMBER_ALREADY_USE(
+            206,
+            "This phone number already using."
+    );
 
-    private int errorCode;
-    private String errorMessage;
+    private final int errorCode;
+    private final String errorMessage;
 
     ErrorMessage(int errorCode, String errorMessage) {
         this.errorCode = errorCode;
